2650dd984d68cef1517d113dede2b4f582b83457
Mow1o <tuomokemi@gmail.com>
2023-01-03 08:36:11 +0200

    First

---
+++ tiltangle.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+import time
+import random
+import json
+
+mqttBroker ="test.mosquitto.org"
+client = mqtt.Client (client_id="Simulaattori771", clean_session=True)
+client.connect(mqttBroker, port=1883)
+i = 1
+while i < 10:
+    i = i+1
+    sensorTemp = random.randint(1, 30)
+    sensorHum = random.randint(1, 100)
+
+
+    # Muutetaan python dictionary Json objektiksi
+    value = {"Temperature":sensorTemp, "Humidity":sensorHum}
+    values = json.dumps(value)
+    type(values)
+    #lähetetään json objekti
+    client.publish("ArkPy/Tila/Sensor", values)
+    print("Published " + str(sensorTemp) + " to topic Temperature" + " and " + str(sensorHum) + " to topic Humidity" )
+
+    time.sleep(5)
+client.disconnect()
